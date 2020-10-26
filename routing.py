26cdc40531081d40af8ebd62f5ea92c8c069b09b
Chad Minning <chad.minning@gmail.com>
2020-10-26 14:51:30 -0600

    Added basic GET functionality to API

--- routing.py
+++ routing.py
@@ -1,16 +1,21 @@
 import sqlite3
 import json
-from flask import Flask
+from flask import Flask, jsonify, Response
 from flask import g
 
 app = Flask(__name__)
 
 DATABASE = './inventory.db'
 
+def make_dicts(cursor, row):
+    return dict((cursor.description[idx][0], value)
+                for idx, value in enumerate(row))
+
 def get_db():
     if hasattr(g, "db"):
         return g.db
     db = g.db = sqlite3.connect(DATABASE)
+    db.row_factory = make_dicts
     return db
 
 @app.teardown_appcontext
@@ -33,7 +38,23 @@ def remove():
 
 
 
-@app.route('/request/') #TODO: add functionality for "request" route
-def request():
-    return "request"
+@app.route('/inventory/<id>', methods=['GET'])    #GET request that returns database records that match name or id number. 
+def inventory(id):
+    db = get_db()
+    cur = db.cursor()
+    cur.execute('select * from inv where id = ? or name = ? COLLATE NOCASE', (id, id))  #Case-insensitive search for either name or id.
+    records = cur.fetchall()    #Fetch all the records retrieved from execute and place them in list of dicts (as specefied by make_dicts() fucntion.)
+    data = json.dumps(records)  #Create json string with the list of records.
+    cur.close()
+    return Response(data, mimetype = 'application/json')    #Format response using JSON MIME type so that the requesting application recognizes it.
 
+
+@app.route('/inventory/all', methods=['GET']) #Same functionality as '/requests/<id>' but instead it returns all records.
+def inventory_all():
+    db = get_db()
+    cur = db.cursor()
+    cur.execute('select * from inv')    #Select all available records from table.
+    records = cur.fetchall()
+    data = json.dumps(records)
+    cur.close()
+    return Response(data, mimetype = 'application/json')    
