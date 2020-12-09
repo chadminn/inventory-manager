e71ff0e9e8b7a4cdb2601895295458952cfa4001
Chad Minning <chad.minning@gmail.com>
2020-12-08 19:58:22 -0700

    Fixed a bug that prevented floating point numbers in the price field.

--- webapp/webapp.py
+++ webapp/webapp.py
@@ -4,31 +4,31 @@ import json
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/') #Renders the index.html page with all the contents of the database included.
 def render():
     req = requests.get('http://127.0.0.1:5000/api/inventory/all')
     data = req.json()
     return render_template('index.html', value = data)
 
-@app.route('/search')
+@app.route('/search')   #Sends a GET request to the API to find a specific ID and returns the results in a table.
 def input():
     id = request.args['id']
     req = requests.get('http://127.0.0.1:5000/api/inventory?id=' + id)
     data = req.json()
     return render_template('index.html', value = data)
 
-@app.route('/delete')
+@app.route('/delete')   #Sends a DELETE request to the API for a particular id.
 def delete():
     id = request.args['id']
     requests.delete('http://127.0.0.1:5000/api/inventory/delete?id=' + id)
     return redirect(url_for('render'));
 
-@app.route('/add', methods=['POST'])
+@app.route('/add', methods=['POST'])    #Sends an add request to the API with form information from the web UI.
 def add():
     name = request.form.get('name')
     price = request.form.get('price')
     stock = request.form.get('stock')
-    if (price.isnumeric() and stock.isnumeric()):   #Check to see if the input values are correct.
+    if (price.replace('.','',1).isdigit() and stock.isdigit()):   #Check to see if the input values are correct.
         obj = {'name' : name, 'price' : price, 'stock' : stock}
         requests.post('http://127.0.0.1:5000/api/inventory/add', data = obj)
         return redirect(url_for('render'))
